b06c314808c2ec36b4865b9e94d116463f57528a
AlumnoMañana <AlumnoMaana@T20211>
2018-11-13 09:55:50 +0000

    Version 1 - Pendiente 13/11/2018

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,11 +1,13 @@
 package principal;
+import java.util.Arrays;
+
 import ejercicios.Ejercicios;
 
 public class Principal {
 	public static void main(String[]args) {
-
 		Ejercicios ejercicios= new Ejercicios();
-		System.out.println(ejercicios.desgloseMoneda(78));
+		monedas =ejercicios.desgloseMoneda(78);
+		System.out.println(Arrays.toString(monedas));
 
 	}
 }
