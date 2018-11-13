a77d817e406d586a9c36e65b291a4fcf46653a33
AlumnoMañana <AlumnoMaana@T20211>
2018-11-13 13:52:07 +0000

    13/11/2018 Activ.Pendientes

--- src/ejercicios/Ejercicios.java
+++ src/ejercicios/Ejercicios.java
@@ -1,15 +1,71 @@
 package ejercicios;
 
+
 public class Ejercicios {
+	/* CORREGIR	
 	public int[]desgloseMoneda(int cantidad) {
 		int[]monedas= {500,200,100,50,20,10,5,2,1};
 		int[]resultado = new int[monedas.length];
 		for (int i = 0; i < monedas.length; i++) {
 			while(cantidad / monedas[i] != 0){
-				resultado[i] = monedas[i];
+				resultado[i] += 1;
 				cantidad = cantidad % monedas[i];
 			}
 		}
 		return resultado;	
 	}
+	
+
+public class Ejercicios {
+	public int[]desgloseMoneda(int cantidad) {
+		int[]monedas= {500,200,100,50,20,10,5,2,1};
+		int[]resultado = new int[monedas.length];
+		int indice=0;
+			while(cantidad % monedas[indice] != 0){
+				resultado[indice] = cantidad / monedas[indice];
+				cantidad = cantidad % monedas[indice];
+				indice++;
+			}
+
+		return resultado;	
+	}
+
+	public int[]listaDesgloseMoneda(int cantidad) {
+		int[]monedas= {500,200,100,50,20,10,5,2,1};
+		int[]resultado =desgloseMoneda(cantidad);
+		for (int i = 0; i < resultado.length; i++) {
+			if(resultado[i]!=0) {
+				System.out.println(monedas[i]+ " ->" + resultado[i]);
+			}
+			
+		}
+		return resultado;
+	}
+
+*/
+
+
+
+	
+	
+	
+	// Actividad: Pasar una lista de números desordenados y mostrarlos ordenados
+	public void ordenarArray(int [] numeros) {
+		int numCompara = numeros[0];
+		for (int i = 0; i < numeros.length-1; i++) {
+			for (int j = 0; j < numeros.length; j++) {
+				if(numeros[i]>numeros[j]) {
+					numCompara = numeros[i];
+					numeros[i]= numeros[j];
+					numeros[j]= numeros[i];	
+				}
+			}
+		}
+	}
+	
+	
+	
+	
+	//Ordenar un array de cadenas
+	
 }
--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -5,9 +5,13 @@ import ejercicios.Ejercicios;
 
 public class Principal {
 	public static void main(String[]args) {
-		Ejercicios ejercicios= new Ejercicios();
-		int monedas[] =ejercicios.desgloseMoneda(78);
-		System.out.println(Arrays.toString(monedas));
+		/*Ejercicios ejercicios= new Ejercicios();
+		int monedas[] =ejercicios.desgloseMoneda(1234);
+		System.out.println(Arrays.toString(monedas));*/
 
+		//int monedas[] =ejercicios.listaDesgloseMoneda(1234);
+		
+		
+		int ordenarArray[]= {40,8,2,1,30};
 	}
 }
