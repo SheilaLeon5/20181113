9f7540eeffb72d710dac94359533d7b52f46f8e2
sheila <sheila@Len>
2018-11-17 11:27:14 +0000

    mejorado - Falta unos pocos cambios

--- src/ejercicios/Ejercicios.java
+++ src/ejercicios/Ejercicios.java
@@ -1,36 +1,39 @@
 package ejercicios;
 
+import java.util.Arrays;
 
 public class Ejercicios {
-	/* CORREGIR	
+	//----------------------ACTIVIDAD: Desglosar una cantidad en billetes y monedas
 	public int[]desgloseMoneda(int cantidad) {
 		int[]monedas= {500,200,100,50,20,10,5,2,1};
 		int[]resultado = new int[monedas.length];
 		for (int i = 0; i < monedas.length; i++) {
 			while(cantidad / monedas[i] != 0){
-				resultado[i] += 1;
+				resultado[i] = cantidad/monedas[i];
 				cantidad = cantidad % monedas[i];
 			}
 		}
 		return resultado;	
 	}
 	
-
-public class Ejercicios {
-	public int[]desgloseMoneda(int cantidad) {
+	
+	/* ---------------------------ACTIVIDAD: Listar desgloseMoneda		13/11/2018---------
+	 * Esta actividad se realizará usando el método anterior "desgloseMoneda" para obtener una lista 
+	 * del número de billetes o monedas utilizados.
+	*/
+	
+	public void listaDesgloseMoneda(int cantidad){
 		int[]monedas= {500,200,100,50,20,10,5,2,1};
-		int[]resultado = new int[monedas.length];
-		int indice=0;
-			while(cantidad % monedas[indice] != 0){
-				resultado[indice] = cantidad / monedas[indice];
-				cantidad = cantidad % monedas[indice];
-				indice++;
+		for (int i = 0; i < monedas.length; i++) {
+			int[] resultado = desgloseMoneda(2850);
+			if (resultado[i] != 0){
+				System.out.println(monedas[i] + "€ -->" + resultado[i]);
 			}
-
-		return resultado;	
+		}
 	}
-
-	public int[]listaDesgloseMoneda(int cantidad) {
+	
+	// ACTIVIDAD DEL PROFESOR (ACTIVIDAD: Listar desgloseMoneda
+/*	public int[]listaDesgloseMoneda(int cantidad) {
 		int[]monedas= {500,200,100,50,20,10,5,2,1};
 		int[]resultado =desgloseMoneda(cantidad);
 		for (int i = 0; i < resultado.length; i++) {
@@ -45,11 +48,8 @@ public class Ejercicios {
 */
 
 
-
-	
-	
-	
-/*	// Actividad: Pasar una lista de números desordenados y mostrarlos ordenados
+/*
+	// Actividad: Pasar una lista de números desordenados y mostrarlos ordenados
 	public void ordenarArray(int [] numeros) {
 		int numCompara = numeros[0];
 		for (int i = 0; i < numeros.length-1; i++) {
@@ -62,12 +62,11 @@ public class Ejercicios {
 			}
 		}
 	}
-	*/
-	
+*/
 	
 	
 	//Ordenar un array de cadenas  14/11/2018
-	public void ordenarCadena(String [] cadenas) {
+/*	public void ordenarCadena(String [] cadenas) {
 		for (int i = 0; i < cadenas.length-1; i++) {
 			for (int j = 0; j < cadenas.length; j++) {
 				if(cadenas[i].compareTo(cadenas[j]) >0) {  // ENTENDER LINEA
@@ -76,8 +75,8 @@ public class Ejercicios {
 					cadenas[j]= comparador;	
 					
 				}
-			}
+			} 
 		}
-	}
+	}*/
 	
 }
--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -5,19 +5,31 @@ import ejercicios.Ejercicios;
 
 public class Principal {
 	public static void main(String[]args) {
-		/*Ejercicios ejercicios= new Ejercicios();
-		int monedas[] =ejercicios.desgloseMoneda(1234);
-		System.out.println(Arrays.toString(monedas));*/
-
-		//int monedas[] =ejercicios.listaDesgloseMoneda(1234);
+		Ejercicios ejercicios= new Ejercicios();
+		ejercicios.listaDesgloseMoneda(1234);
 		
+	/*
+	 * 
+	 * 
+	 //----------------------ACTIVIDAD: Desglosar una cantidad en billetes y monedas
+		Ejercicios ejercicios= new Ejercicios();
+		int monedas[] =ejercicios.desgloseMoneda(1553);
+		System.out.println(Arrays.toString(monedas));
+		
+		
+	 //-----------------------ACTIVIDAD: Listar desgloseMoneda		13/11/2018---------
+	  	Ejercicios ejercicios= new Ejercicios();
+		ejercicios.listaDesgloseMoneda(1234);
+	
+	*/	
 		
 		//int ordenarArray[]= {40,8,2,1,30};
 		
 		
 		
-		Ejercicios ejercicios= new Ejercicios();
+/*		Ejercicios ejercicios= new Ejercicios();
 		String palabras[]= {"zorro","azul","abeja","pez"};
 		System.out.println(Arrays.toString(palabras));
+*/
 	}
 }
